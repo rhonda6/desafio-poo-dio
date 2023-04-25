8c857f5a3a7bbd127f67b04fb04eabc6e2b6b5d1
rhsilva06 <110588666+rhsilva06@users.noreply.github.com>
2023-04-25 07:34:38 -0300

    Abstração e encapsulamento

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descrição do Curso Javascript");
+        curso2.setCargaHoraria(8);
+
+        System.out.println(curso1.toString());
+        System.out.println(curso2.toString());
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria.toString());
     }
 }
